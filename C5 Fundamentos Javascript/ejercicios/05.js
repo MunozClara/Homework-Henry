1762924b0f5d498dd6a45521115a0748cca3f41d
Clari <clarii.20@hotmail.com>
2024-07-30 22:48:25 -0300

    ejercicio 5 modificado

--- C5 Fundamentos Javascript/ejercicios/05.js
+++ C5 Fundamentos Javascript/ejercicios/05.js
@@ -1,6 +1,7 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevaMultiplicacion = 10 * 4 === 40;
+const nuevaMultiplicacion = 10 * 5 === 40;
+console.log(nuevaMultiplicacion)
 
 module.exports = nuevaMultiplicacion;
