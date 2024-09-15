a5cc8181bb5487d7b290ee2a95dc3a6cd2296cad
MunozClara <clarii.20@hotmail.com>
2024-09-15 11:52:56 -0300

    Primeros Ejercicios de Bucles

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -1,7 +1,22 @@
+
 function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
-  // Tu código:
+  let total = 0;
+
+  for (let i = a; i <= b; i++) {
+    total = total * i;
+    // Si encuentras un 0 en el rango, el producto es 0
+    if (i === 0) {
+      return 0;
+    }
+  }
+
+  return total;
 }
+
+
+
+
 
 module.exports = productoEntreNúmeros;
