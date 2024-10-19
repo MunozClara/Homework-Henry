866512913e04da84acef448f06871ebd2ba06e0c
ariel1092 <fede_10_92@hotmail.com>
2024-10-19 12:43:01 -0300

    prueba del primer ejercicio (con error)

--- C8 Objects + Callbacks/callbacks/ejercicios/01.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/01.js
@@ -2,6 +2,13 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // NOTA: no debes retornar nada.
    // Tu código:
+   const test = {
+      prop: 42,
+      func: function () {
+      return this.prop;
+      },
+   }; 
+   console.log(test.func());
 }
 
 module.exports = invocarCallback;
